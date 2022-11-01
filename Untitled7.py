ef9e7c5145f95ef457307aa8eb0dbde12768fb48
Rafael-RJ-21 <98595734+Rafael-RJ-21@users.noreply.github.com>
2022-11-01 15:26:00 -0300

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+# **Rafael Ferreira Soares**
+# University student of Accounting
+# _I am studuying Python, because I want to work with data. Accounting is data, so I think that learning this code language, I can be more prepared to the job_
+# 
+
+# In[1]:
+
+
+def gauss(n):
+    return(n*(n+1))/2
+gauss(100)  == sum(range(101)) 
+
